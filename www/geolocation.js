ce9119e01ab66751735628cf7725512d0429f6d5
DuongKien <ddk0705@gmail.com>
2017-10-08 21:13:43 +0700

    Fix geolocation.js

--- www/geolocation.js
+++ www/geolocation.js
@@ -1,17 +1,17 @@
 var exec = require('cordova/exec');
 
-// exports.coolMethod = function(arg0, success, error) {
-//     exec(success, error, "geolocation", "coolMethod", [arg0]);
-// };
-
-exports.echo = function(arg0, success, error) {
-    exec(success, error, "geolocation", "coolMethod", [arg0]);
+exports.getstatus = function(arg0, success, error) {
+    exec(success, error, "geolocation", "getstatus", [arg0]);
 };
 
-exports.echojs = function(arg0, success, error) {
+exports.getname = function(arg0, success, error) {
   if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
     success(arg0);
-  } else {
+  }else {
     error('Empty message!');
   }
 };
+
+exports.getgeolocation = function(arg0, success, error) {
+  exec(success, error, "geolocation", "getgeolocation", [arg0]);
+}
